edebcecc49b4861b212aebd0605fef0552c22dfb
barrj <barr@ithaca.edu>
2024-07-22 08:51:44 -0400

    Delete authenticatIon directory

--- authenticatIon/pages/Slides.js
+++
@@ -1,9 +0,0 @@
-// Desktops
-const Slides = () => (
-  <div>
-    <h2>Slides</h2>
-    <p>Slides Page</p>
-  </div>
-);
-
-export default Slides;
--- authenticatIon/pages/Videos.js
+++
@@ -1,9 +0,0 @@
-// Laptops
-const Videos = () => (
-  <div>
-    <h2>Videos</h2>
-    <p>Videos Page</p>
-  </div>
-);
-
-export default Videos;
